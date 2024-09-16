ee4e8bf96cd82254bab5e9d4bf917bc7d0e7ca69
leonardotc <ltc270486@hotmail.com>
2024-09-16 11:25:32 +0200

    Change to class syntax

--- src/signer_backend/src/index.ts
+++ src/signer_backend/src/index.ts
@@ -1,26 +1,14 @@
-import { Canister, text, query, update, Void, serialize, Opt, None, Some } from 'azle/experimental'
+// import { text, query, update, Void, serialize } from 'azle/experimental'
 import { EcdsaPublicKeyResult } from 'azle/canisters/management'
 import createKeccakHash from 'keccak'
+import { query, IDL, update } from 'azle'
+import { serialize } from 'azle/experimental'
 
 let publicKey: Uint8Array
 
-export default Canister({
-    update: update([], Void, async () => {
-        // Very beautiful but it doesnt work
-        // const rawResponse = await call(
-        //     PRINCIPAL,
-        //     FUNCS.ecdsa_public_key,
-        //     {
-        //         args: [{
-        //             canister_id: [],
-        //             derivation_path: [],
-        //             key_id: {
-        //                 curve: { secp256k1: null },
-        //                 name: 'dfx_test_key'
-        //             }
-        //         }]
-        //     }
-        // )
+export default class Canister{
+    @update([])
+    async update(): Promise<void> {
         const rawResponse = await fetch('icp://aaaaa-aa/ecdsa_public_key', {
             body: serialize({
                 args: [
@@ -37,8 +25,11 @@ export default Canister({
         })
         const response: EcdsaPublicKeyResult = await rawResponse.json();
         publicKey = response.public_key
-    }),
-    address: query([], text, async () => {
+    }
+
+    @query([], IDL.Text)
+    async address(): Promise<string> {
+        // Verify costs, its ugly like this because I am assuming update is paid while query aint.
         return `0x${createKeccakHash('keccak256').update(Buffer.from(publicKey)).digest('hex').slice(-40)}`
-    })
-})
+    }
+}
